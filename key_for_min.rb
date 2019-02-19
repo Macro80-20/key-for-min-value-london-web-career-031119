58100d3874175ca3fdce4e761dfd93b7babe1352
fernando <fernando_lopezc@live.com>
2019-02-19 08:58:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+#veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+#key_for_min_value(veggies)
 
 def key_for_min_value(name_hash)
-
+name_hash.each do
+  |key| #
+  key
+end
 end
